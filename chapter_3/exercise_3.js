ecac59c42d8c3bae5f4db09c5da54ea0137716a5
luqmanbrainz97 <hakimbrainz97@gmail.com>
2020-08-09 22:50:08 +0800

    Chapter 3 Question 1

--- chapter_3/exercise_3.js
+++ chapter_3/exercise_3.js
@@ -4,19 +4,18 @@
  number that indicates how many uppercase “B” characters there are in the string.
  */
 
-// var z = "BBC";
-// console.log(z[1]);
+function countBs(word) {
+  let counter = 0;
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === "B") {
+      counter += 1;
+    }
+  }
+  return counter;
+}
 
-// function countBs(word) {
-//   let display = "";
-//   for (var i = 0; i < word.length; i++) {
-//     display += word[i];
-//   }
-//   return typeof display;
-// }
+console.log(countBs("BBC"));
 
-// console.log(countBs("BBC"));
+// var i = [{ events: 0 }, { events: 1 }, { events: 2 }];
 
-var i = [{ events: 0 }, { events: 1 }, { events: 2 }];
-
-console.log(i[1]);
+// console.log(i[1]);
